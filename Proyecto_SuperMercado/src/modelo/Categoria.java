ba242578c72c8475f7d03a601eb0f56dfd5f75d1
BryanAndres25 <47094533+BryanAndres25@users.noreply.github.com>
2019-06-24 15:37:32 -0500

    Add files via upload

---
+++ Proyecto_SuperMercado/src/modelo/Categoria.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Categoria {
+    private String cat_nombre;
+    private int cat_id;
+
+    public String getCat_nombre() {
+        return cat_nombre;
+    }
+
+    public void setCat_nombre(String cat_nombre) {
+        this.cat_nombre = cat_nombre;
+    }
+
+    public int getCat_id() {
+        return cat_id;
+    }
+
+    public void setCat_id(int cat_id) {
+        this.cat_id = cat_id;
+    }
+    
+    
+    
+    
+            
+}
