88149c34e29d595c6992bae07b20cea9ec358277
BryanAndres25 <47094533+BryanAndres25@users.noreply.github.com>
2019-06-24 15:46:26 -0500

    Add files via upload

---
+++ Proyecto_SuperMercado/src/modelo/Proveedor.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Proveedor {
+    private String prov_ruc,prov_nombre_empresa;
+    private int prov_id,prov_cantidad;
+
+    public String getProv_ruc() {
+        return prov_ruc;
+    }
+
+    public void setProv_ruc(String prov_ruc) {
+        this.prov_ruc = prov_ruc;
+    }
+
+    public String getProv_nombre_empresa() {
+        return prov_nombre_empresa;
+    }
+
+    public void setProv_nombre_empresa(String prov_nombre_empresa) {
+        this.prov_nombre_empresa = prov_nombre_empresa;
+    }
+
+    public int getProv_id() {
+        return prov_id;
+    }
+
+    public void setProv_id(int prov_id) {
+        this.prov_id = prov_id;
+    }
+
+    public int getProv_cantidad() {
+        return prov_cantidad;
+    }
+
+    public void setProv_cantidad(int prov_cantidad) {
+        this.prov_cantidad = prov_cantidad;
+    }
+    
+    
+    
+}
