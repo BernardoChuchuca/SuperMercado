dbaf59b9fd83d74f9d0d00964e16bb6d91f40ba8
BernardoChuchuca <47090582+BernardoChuchuca@users.noreply.github.com>
2019-07-04 15:12:37 -0500

    Add files via upload

--- Proyecto_SuperMercado/src/vista/AnularFactura.java
+++ Proyecto_SuperMercado/src/vista/AnularFactura.java
@@ -54,6 +54,7 @@ public class AnularFactura extends javax.swing.JInternalFrame {
         tabledet = new javax.swing.JTable();
         jfac_id = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -81,6 +82,13 @@ public class AnularFactura extends javax.swing.JInternalFrame {
 
         jLabel2.setText("ID :");
 
+        jButton1.setText("ANULAR");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -94,9 +102,12 @@ public class AnularFactura extends javax.swing.JInternalFrame {
                         .addComponent(jcedula, javax.swing.GroupLayout.DEFAULT_SIZE, 104, Short.MAX_VALUE)
                         .addGap(29, 29, 29))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(28, 28, 28)
-                        .addComponent(jfac_id, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jButton1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(28, 28, 28)
+                                .addComponent(jfac_id, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 570, Short.MAX_VALUE)
@@ -119,6 +130,8 @@ public class AnularFactura extends javax.swing.JInternalFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jfac_id, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(33, 33, 33)
+                        .addComponent(jButton1)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
@@ -152,6 +165,28 @@ public class AnularFactura extends javax.swing.JInternalFrame {
              
         }
     }//GEN-LAST:event_jcedulaKeyTyped
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+       ArrayList<Detalle_Factura>lx=new GestionFacturasDetalle().getListDetalleFacturaid(Integer.parseInt(jfac_id.getText()));
+       
+        for (int i = 0; i < lx.size(); i++) {
+            
+            Producto pro=new GestionProductos().getBuscarPoductoid(lx.get(i).getPro_id());
+            int stock=pro.getPro_stock();
+            pro.setPro_stock((int) (stock+lx.get(i).getDet_catidad()));
+            new GestionProductos().ActualizarProducto(pro);
+            Factura fac =new GestionFactura().getListFacturaid(lx.get(i).getFac_id());
+            fac.setFact_estado("Z");
+            new GestionFactura().ActualizarFactura(fac);
+            
+            
+                    
+            
+        }
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -190,6 +225,7 @@ public class AnularFactura extends javax.swing.JInternalFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
